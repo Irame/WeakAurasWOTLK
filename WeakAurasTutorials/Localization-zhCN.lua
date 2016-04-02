69f343a327563aacc542ce97f0717c3d3bbd270d
Irame <irame94@gmail.com>
2016-04-02 05:00:16 +0200

    initial commit

---
+++ WeakAurasTutorials/Localization-zhCN.lua
@@ -0,0 +1,10 @@
+﻿if not(GetLocale() == "zhCN") then
+    return;
+end
+
+local L = WeakAuras.L
+
+-- Options translation
+--@localization(locale="znCN", format="lua_additive_table", namespace="WeakAuras / Tutorials")@
+
+
